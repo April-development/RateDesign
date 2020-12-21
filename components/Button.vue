01816158dfa88594c6f5eac1b3c23f5ace963046
Александр Заверняев <zaverclub@gmail.com>
2020-12-21 20:03:48 +0300

    Добавлены кнопки

---
+++ components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<style scoped lang="sass">
+@import "assets/colors"
+.button
+  color: white
+
+  background-color: $black-100
+
+  width: 328px
+  height: 48px
+
+  border-radius: 8px
+  &:active
+    background-color: $black-90
+  &:disabled
+    background-color: $black-40
+  @media (min-width: 414px)
+    width: 384px
+</style>
